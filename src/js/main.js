26c33f9130043466de303f770cee6252f0603b61
dimichkyiv <dimichmf1995@gmail.com>
2022-01-19 12:16:55 +0200

    modal

--- src/js/main.js
+++ src/js/main.js
@@ -99,14 +99,12 @@ const modalCloseBtn = document.querySelector('[data-close]');
 
 modalTrigger.forEach(btn => {
     btn.addEventListener('click', () =>{
-        modal.classList.add('show');
-        modal.classList.remove('hide');
+        modal.style.display = 'block';
         document.body.style.overflow = "hidden";
     });
 });
 function closeModal () {
-    modal.classList.add('hide');
-    modal.classList.remove('show');
+    modal.style.display = 'none';
     document.body.style.overflow = "";
 }
 modalCloseBtn.addEventListener('click', closeModal);
